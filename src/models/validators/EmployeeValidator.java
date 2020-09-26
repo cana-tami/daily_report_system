266e5b039ca027afcad9b5976cd3654dab6f0293
kana.tamichi <kana.tamichi624@gmail.com>
2020-09-26 20:24:04 +0900

    Add Employee index

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -63,6 +63,7 @@ public class EmployeeValidator {
 
 	//password check
 	private static String _validatePassword(String password, Boolean password_check_flag) {
+		//パスワードを変更する場合のみ実行
 		if(password_check_flag && (password == null || password.equals(""))) {
 			return "パスワードを入力してください。";
 		}
